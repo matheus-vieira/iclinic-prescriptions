8099c2d14fb673a1c05d3463d8ccbdb6e22a21a9
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2020-09-03 14:29:15 -0300

    remove duplicated code

--- test/prescription.js
+++ test/prescription.js
@@ -25,6 +25,16 @@ const checkDB = async (response) => {
   expect(JSON.stringify(response.body)).to.be.equal(JSON.stringify(expected));
 };
 
+function malFormed() {
+  const response = await callPrescriptions(validData);
+
+    expect(response.status).to.be.equal(400);
+    expect(response.body).to.be.an("object");
+    expect(response.body.error).to.be.an("object");
+    expect(response.body.error.message).to.be.equal("malformed request");
+    expect(response.body.error.code).to.be.equal("01");
+}
+
 describe("Prescription's endpoint", () => {
   let validData, server;
   const callPrescriptions = async (data) => {
@@ -54,46 +64,22 @@ describe("Prescription's endpoint", () => {
 
   it("should show a malformed request without clinic on request", async () => {
     delete validData.clinic;
-    const response = await callPrescriptions(validData);
-
-    expect(response.status).to.be.equal(400);
-    expect(response.body).to.be.an("object");
-    expect(response.body.error).to.be.an("object");
-    expect(response.body.error.message).to.be.equal("malformed request");
-    expect(response.body.error.code).to.be.equal("01");
+    
   }, 30000);
 
   it("should show a malformed request without physician on request", async () => {
     delete validData.physician;
-    const response = await callPrescriptions(validData);
-
-    expect(response.status).to.be.equal(400);
-    expect(response.body).to.be.an("object");
-    expect(response.body.error).to.be.an("object");
-    expect(response.body.error.message).to.be.equal("malformed request");
-    expect(response.body.error.code).to.be.equal("01");
+    malFormed();
   }, 30000);
 
   it("should show a malformed request without patient on request", async () => {
     delete validData.patient;
-    const response = await callPrescriptions(validData);
-
-    expect(response.status).to.be.equal(400);
-    expect(response.body).to.be.an("object");
-    expect(response.body.error).to.be.an("object");
-    expect(response.body.error.message).to.be.equal("malformed request");
-    expect(response.body.error.code).to.be.equal("01");
+    malFormed();
   }, 30000);
 
   it("should show a malformed request without text on request", async () => {
     delete validData.text;
-    const response = await callPrescriptions(validData);
-
-    expect(response.status).to.be.equal(400);
-    expect(response.body).to.be.an("object");
-    expect(response.body.error).to.be.an("object");
-    expect(response.body.error.message).to.be.equal("malformed request");
-    expect(response.body.error.code).to.be.equal("01");
+    malFormed();
   }, 30000);
 
   it("should be succes with wrong clinic id", async () => {
