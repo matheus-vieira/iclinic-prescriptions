5be81dad3b138a36200ce0891bb2933c202b6ba5
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2022-12-08 14:49:22 -0300

    refactor test code (#35)

--- test/prescription.js
+++ test/prescription.js
@@ -25,20 +25,23 @@ const checkDB = async (response) => {
   expect(JSON.stringify(response.body)).to.be.equal(JSON.stringify(expected));
 };
 
-const malFormed = async () => {
-  const response = await callPrescriptions(validData);
-
-  expect(response.status).to.be.equal(400);
-  expect(response.body).to.be.an("object");
-  expect(response.body.error).to.be.an("object");
-  expect(response.body.error.message).to.be.equal("malformed request");
-  expect(response.body.error.code).to.be.equal("01");
-};
-
 describe("Prescription's endpoint", () => {
   let validData, server;
+
   const callPrescriptions = async (data) => {
-    return await request(server).post("/v2/prescriptions").send(data);
+    return await request(server)
+      .post("/v2/prescriptions")
+      .send(data);
+  };
+
+  const malFormed = async () => {
+    const response = await callPrescriptions(validData);
+  
+    expect(response.status).to.be.equal(400);
+    expect(response.body).to.be.an("object");
+    expect(response.body.error).to.be.an("object");
+    expect(response.body.error.message).to.be.equal("malformed request");
+    expect(response.body.error.code).to.be.equal("01");
   };
 
   beforeEach(function () {
@@ -62,26 +65,22 @@ describe("Prescription's endpoint", () => {
     await checkDB(response);
   }, 30000);
 
-  it("should show a malformed request without clinic on request", async () => {
-    delete validData.clinic;
+  const malformedTheories = [
+    "clinic", "physician", "patient", "text"
+  ];
 
-    malFormed();
-  }, 30000);
-
-  it("should show a malformed request without physician on request", async () => {
-    delete validData.physician;
-    malFormed();
-  }, 30000);
-
-  it("should show a malformed request without patient on request", async () => {
-    delete validData.patient;
-    malFormed();
-  }, 30000);
-
-  it("should show a malformed request without text on request", async () => {
-    delete validData.text;
-    malFormed();
-  }, 30000);
+  malformedTheories.forEach(prop => {
+    const validDataMalFormed = {
+      clinic: { id: 1 },
+      physician: { id: 1 },
+      patient: { id: 1 },
+      text: "Dipirona 1x ao dia",
+    };
+    it(`should show a malformed request without ${prop} on request`, async () => {
+      delete validDataMalFormed[prop];
+      malFormed();
+    }, 30000);
+  });
 
   it("should be succes with wrong clinic id", async () => {
     const response = await callPrescriptions({
